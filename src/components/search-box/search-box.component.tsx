0369acd890f08b0ef62ecc0a0737ddd1e8f7dde0
Michael Cruz <mrcruz1989@gmail.com>
2023-11-15 16:39:20 -0500

    added note about ChangeEventHandler vs ChangeEvent

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,10 +1,20 @@
 import "./search-box.styles.css";
-import { ChangeEventHandler } from "react";
+import {
+  ChangeEventHandler,
+  // ChangeEvent
+} from "react";
 
 type SearchBoxProps = {
   placeholder?: string;
   className: string;
   handleSearchChange: ChangeEventHandler<HTMLInputElement>;
+  /*
+  Below is another way to define the type of the function.
+  This allows us to specify the type of output of the function.
+  Normally not needed since ChangeEventHandler is almost always void.
+
+  handleSearchChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  */
 };
 
 const SearchBox = ({
